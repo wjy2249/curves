2380d11e7f1bdeb33ada91ced2eb0ad9b274ca33
link <2453692195@qq.com>
2024-01-08 15:54:32 +0800

    增加了Bspline曲面

--- Bspline/Bspline3D.hpp
+++ Bspline/Bspline3D.hpp
@@ -87,3 +87,91 @@ private:
     int degree_;
     std::vector<double> knots_;
 };
+
+class BSplineSurface3D
+{
+public:
+    explicit BSplineSurface3D(const int& degreeU, const int& degreeV, const std::vector<std::vector<Point3D>>& controlPoints, const std::vector<double>& knotsU, const std::vector<double>& knotsV)
+        : degreeU_(degreeU), degreeV_(degreeV), controlPoints(controlPoints), knotsU_(knotsU), knotsV_(knotsV)
+    {
+        nU_ = controlPoints.size() - 1;
+        nV_ = controlPoints[0].size() - 1;
+    }
+
+    inline Point3D evaluate(double u, double v) const
+    {
+        int spanU = findSpan(u, nU_, degreeU_, knotsU_);
+        int spanV = findSpan(v, nV_, degreeV_, knotsV_);
+        std::vector<double> basisU = computeBasis(spanU, u, nU_, degreeU_, knotsU_);
+        std::vector<double> basisV = computeBasis(spanV, v, nV_, degreeV_, knotsV_);
+        Point3D result = { 0.0, 0.0, 0.0 };
+
+        for (int i = 0; i <= degreeU_; ++i)
+        {
+            for (int j = 0; j <= degreeV_; ++j)
+            {
+                double basisMultiplier = basisU[i] * basisV[j];
+                result.x += controlPoints[spanU - degreeU_ + i][spanV - degreeV_ + j].x * basisMultiplier;
+                result.y += controlPoints[spanU - degreeU_ + i][spanV - degreeV_ + j].y * basisMultiplier;
+                result.z += controlPoints[spanU - degreeU_ + i][spanV - degreeV_ + j].z * basisMultiplier;
+            }
+        }
+
+        return result;
+    }
+
+private:
+    int findSpan(double t, int n, int degree, const std::vector<double>& knots) const
+    {
+        int low = degree;
+        int high = n + 1;
+        int mid;
+
+        while (high - low > 1)
+        {
+            mid = (high + low) >> 1;
+
+            if (t < knots[mid] - eps)
+                high = mid;
+            else
+                low = mid;
+        }
+
+        return low;
+    }
+
+    std::vector<double> computeBasis(int span, double t, int n, int degree, const std::vector<double>& knots) const
+    {
+        std::vector<double> basis(degree + 1, 0.0);
+        std::vector<double> left(degree + 1, 0.0);
+        std::vector<double> right(degree + 1, 0.0);
+
+        basis[0] = 1.0;
+
+        for (int j = 1; j <= degree; ++j)
+        {
+            left[j] = t - knots[span + 1 - j];
+            right[j] = knots[span + j] - t;
+
+            double saved = 0.0;
+
+            for (int r = 0; r < j; ++r)
+            {
+                double basisMultiplier = basis[r] / (right[r + 1] + left[j - r]);
+                basis[r] = saved + right[r + 1] * basisMultiplier;
+                saved = left[j - r] * basisMultiplier;
+            }
+
+            basis[j] = saved;
+        }
+
+        return basis;
+    }
+
+private:
+    std::vector<std::vector<Point3D>> controlPoints;
+    int nU_, nV_; // control points - 1    
+    int degreeU_, degreeV_;
+    std::vector<double> knotsU_;
+    std::vector<double> knotsV_;
+};
--- test.cpp
+++ test.cpp
@@ -1,26 +1,66 @@
 #include<cstdio>
 #include<vector>
-#include"NURBS/NURBS3D.hpp"
+#include<iostream>
+#include"Bspline/Bspline3D.hpp"
 #define eps 1e-7
 
 int main() 
 {
-    std::vector<Point3D> controlPoints = {
-        { 0.0, 0.0, 0.0 },
-        { 1.0, 2.0, 3.0 },
-        { 3.0, 2.0, 1.0 },
-        { 4.0, 0.0, 0.0 }
+ //   std::vector<Point3D> controlPoints = {
+ //       {0.25212955, 0.20192656, 0.73066454},
+ //       {0.07040837, 0.04212473, 0.14618082},
+ //       {0.80285129, 0.90044722, 0.02047026},
+ //       {0.54550146, 0.31983326, 0.44662081},
+ //       {0.91162146, 0.9554301,  0.72781836},
+ //       {0.0466265,  0.8317827,  0.25163542},
+ //       {0.58953454, 0.80531284, 0.4129208},
+ //       {0.50378757, 0.55654583, 0.46313412},
+ //       {0.43309917, 0.5984493,  0.49776652},
+ //       {0.67352757, 0.68633497, 0.47716614}
+ //   };
+
+ //   std::vector<double> knots = { 0.         ,0.       ,  0.        , 0.     ,    0.1634004  ,0.29624581,
+ //0.30784826, 0.61157454 ,0.62200473 ,0.83257783 ,1.,        1.,
+ //1. ,        1. };
+
+ //   BSplineCurve3D curve(3, controlPoints, knots);
+ //   for (double i = 0; i <= 1-eps ; i += 0.01)
+ //   {
+ //       Point3D p = curve.evaluate(i);
+ //       printf("%f %f %f\n", p.x, p.y, p.z);
+ //   }
+
+    std::vector<std::vector<Point3D>> controlPoints = {
+    {{0, 0, 0}, {1, 0, 0}, {2, 0, 0}},
+    {{0, 1, 0}, {1, 1, 1}, {2, 1, 0}},
+    {{0, 2, 0}, {1, 2, 0}, {2, 2, 0}}
     };
 
-    std::vector<double> knots = { 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0 };
+    // 定义节点向量  
+    std::vector<double> knotsU = { 0, 0, 0, 1, 1, 1 };
+    std::vector<double> knotsV = { 0, 0, 0, 1, 1, 1 };
 
-    std::vector<double> weights = { 1.0, 2.0, 2.0, 1.0 };
+    // 创建 B 样条曲面对象  
+    BSplineSurface3D surface(2, 2, controlPoints, knotsU, knotsV);
 
-    NURBSCurve3D curve(2, controlPoints, knots, weights);
-    for (double i = 0; i <= 1-eps ; i += 0.01)
+    // 测试曲面上的一些点  
+    Point3D point1 = surface.evaluate(0.5, 0.5);
+    Point3D point2 = surface.evaluate(0.3, 0.7);
+    Point3D point3 = surface.evaluate(1.0, 0.0);
+
+    // 打印结果  
+    std::cout << "Point 1: " << point1.x << ", " << point1.y << ", " << point1.z << std::endl;
+    std::cout << "Point 2: " << point2.x << ", " << point2.y << ", " << point2.z << std::endl;
+    std::cout << "Point 3: " << point3.x << ", " << point3.y << ", " << point3.z << std::endl;
+
+    for (double i = 0; i <= 1; i += 0.1)
     {
-        Point3D p = curve.evaluate(i);
-        printf("%f %f %f\n", p.x, p.y, p.z);
+        for (double j = 0; j <= 1; j += 0.1)
+        {
+            Point3D p = surface.evaluate(i, j);
+            std::cout << "i: " << i << " j: " << j <<" 坐标: " << p.x << ", " << p.y << ", " << p.z << std::endl;
+        }
     }
+
     return 0;
 }
